53d600d56987855c507f250ed0978c91770b0aba
meows <b5c6@protonmail.com>
2020-03-03 05:16:35 -0600

    Development settings
    Signed-off-by: meows <b5c6@protonmail.com>

--- ci/crawl.sh
+++ ci/crawl.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-devp2p discv4 crawl -timeout "$ETH_DNS_DISCV4_CRAWLTIME" all.json
+# devp2p discv4 crawl -timeout "$ETH_DNS_DISCV4_CRAWLTIME" all.json
+devp2p --help && echo "" >> all.json
 
 git add all.json	
 
