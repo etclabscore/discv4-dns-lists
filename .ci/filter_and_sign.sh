33d9457e90668cee4009da1cdc6b24106a8e237a
meows <b5c6@protonmail.com>
2021-06-07 12:10:02 -0500

    .ci: add snap protocol support
    Date: 2021-06-07 12:10:02-05:00
    Signed-off-by: meows <b5c6@protonmail.com>

--- .ci/filter_and_sign.sh
+++ .ci/filter_and_sign.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-proto_groups=(all les)
+proto_groups=(all les snap)
 
 for network in "$@"; do
 
@@ -14,19 +14,21 @@ for network in "$@"; do
     mkdir -p "les.${network}.${ETH_DNS_DISCV4_PARENT_DOMAIN}"
     devp2p nodeset filter all.json -les-server -eth-network "$network" >"les.${network}.${ETH_DNS_DISCV4_PARENT_DOMAIN}/nodes.json"
 
-    
+    mkdir -p "snap.${network}.${ETH_DNS_DISCV4_PARENT_DOMAIN}"
+    devp2p nodeset filter all.json -snap -eth-network "$network" >"snap.${network}.${ETH_DNS_DISCV4_PARENT_DOMAIN}/nodes.json"
+
     echo "Sign: $network"
 
-    for p in "${proto_groups[@]}"; do
-        echo -n "Sign: ${p}.${network}.${ETH_DNS_DISCV4_PARENT_DOMAIN}"
+    for proto in "${proto_groups[@]}"; do
+        echo -n "Sign: ${proto}.${network}.${ETH_DNS_DISCV4_PARENT_DOMAIN}"
 
         # Ensure that we actually have a nodeset to sign.
-        [ ! -d ${p}.${network}.${ETH_DNS_DISCV4_PARENT_DOMAIN} ] || [ ! -f ${p}.${network}.${ETH_DNS_DISCV4_PARENT_DOMAIN}/nodes.json ] && { echo " | DNE, skipping"; continue; }
+        [ ! -d ${proto}.${network}.${ETH_DNS_DISCV4_PARENT_DOMAIN} ] || [ ! -f ${proto}.${network}.${ETH_DNS_DISCV4_PARENT_DOMAIN}/nodes.json ] && { echo " | DNE, skipping"; continue; }
 
         echo
-        cat "${ETH_DNS_DISCV4_KEYPASS_PATH}" | devp2p dns sign "${p}.${network}.${ETH_DNS_DISCV4_PARENT_DOMAIN}" "${ETH_DNS_DISCV4_KEY_PATH}" && echo "OK"
+        cat "${ETH_DNS_DISCV4_KEYPASS_PATH}" | devp2p dns sign "${proto}.${network}.${ETH_DNS_DISCV4_PARENT_DOMAIN}" "${ETH_DNS_DISCV4_KEY_PATH}" && echo "OK"
 
-        git add "${p}.${network}.${ETH_DNS_DISCV4_PARENT_DOMAIN}"
+        git add "${proto}.${network}.${ETH_DNS_DISCV4_PARENT_DOMAIN}"
     done
 
     ETH_DNS_DISCV4_KEY_PUBLICINFO="$(cat $ETH_DNS_DISCV4_KEYPASS_PATH | ethkey inspect $ETH_DNS_DISCV4_KEY_PATH | grep -E '(Addr|Pub)')"
